4ad5bbaf71b6b5f836cffae27cf983cbfad8d989
Work Experience <hadi.jawad.softwire@gmail.com>
2017-12-12 16:31:00 +0000

    we have added an if

--- example.js
+++ example.js
@@ -8,8 +8,10 @@ button.onclick = function() {
 	var input1 = document.querySelector("#question1").value;
 	var input2 = document.querySelector("#question2").value;
 // var input= document.querySelector("#question").value;
-
-
+}
+var nameField = document.querySelector("#nameinput");
+nameField.oninput = function() {
+	document.querySelector("colorchange").style.color = colorField.value;
 
 var result = input1 * input2;
 document.querySelector("#answer").innerHTML = result;
